e91ac7c354674d42c774fcb7f36c5c7793a6a129
0bs1d1anC1ph3r <127700025+0bs1d1anC1ph3r@users.noreply.github.com>
2025-03-27 19:43:31 -0400

    Add files via upload

--- CryptoUtils.java
+++ CryptoUtils.java
@@ -1,13 +1,21 @@
 package obs1d1anc1ph3r.reverseshell;
 
-import javax.crypto.*;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.security.*;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 
 public class CryptoUtils {
 
